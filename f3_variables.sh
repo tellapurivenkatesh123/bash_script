5b9182f06a2d113a6366ab0a22102584555cee0c
tellapurivenkatesh123 <tellapurivenkatesh6635@gmail.com>
2025-08-01 00:48:39 +0530

    first commit

---
+++ f3_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#user define variables
+name="venkatesh" #string
+name2=warlu
+echo the name is : $name$name2
+
+value=69.0 #integer
+echo the value is : $value
+
+char='v'
+echo the character is : $char
