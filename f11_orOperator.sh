d9905b4a3b5ea271f8cbf7bacc4127c039c120bc
tellapurivenkatesh123 <tellapurivenkatesh6635@gmail.com>
2025-09-09 22:23:38 +0530

    OR Operator

---
+++ f11_orOperator.sh
@@ -0,0 +1,31 @@
+# #! /bin/bash
+# echo -e "Enter the age: \c"
+# read age
+# if [ "$age" -eq 18 ] || [ "$age" -gt 18 ]
+# then
+#        echo "age is valid"
+# else
+#  	echo "age is invalid"
+# fi	
+
+
+# #! /bin/bash
+# echo -e "Enter the age: \c"
+# read age
+# if [[ "$age" -eq 18 || "$age" -gt 18 ]]
+# then 
+# 	echo "age is valid"
+# else
+# 	echo "age is invalid"
+# fi
+
+
+# #! /bin/bash
+# echo -e "Enter the age: \c"
+# read 
+# if [ "$REPLY" -eq 18 -o "$REPLY" -gt 18 ]
+# then
+# 	echo "age is valid"
+# else 
+# 	echo "age is invalid"
+# fi
