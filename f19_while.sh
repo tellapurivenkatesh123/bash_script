b6c6e017cf90849ee74c62455400442574c00d54
tellapurivenkatesh123 <tellapurivenkateh6635@gmail.com>
2025-09-29 21:54:49 +0530

    all

---
+++ f19_while.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+#while loop
+:<< 'E'
+while condition
+do 
+.
+.
+done
+E
+
+# while read p
+# do                #reading the file using while loop and redirection "<"
+#     echo "$p"
+# done < f1_hello.sh
+
+# cat f1_hello.sh | while read p
+# do 
+#     echo $p
+# done          #readinng the file using pipe "|"
+
+#reding file using IFS 
+
+while IFS= read -r p # "-r flag is used for previnting formate specifiers by interpreter  ->> "\c %d"
+do
+    echo $p
+done < /etc/newt/palette
+
+
--- f1_hello.sh
+++ f1_hello.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "hello world"
+
+echo -e "hello world \c"
