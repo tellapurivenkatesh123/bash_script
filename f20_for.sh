2d4c15d17b507aafe96893952521734e6698c6fe
tellapurivenkatesh123 <tellapurivenkateh6635@gmail.com>
2025-10-01 22:45:12 +0530

    all

---
+++ f20_for.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+#for loop Syntax
+:<<'end'
+for variable in codition 
+do
+    statements
+done
+end
+
+# for i in 1 2 3 4 5 6
+# do
+#     echo $i
+# done
+
+# for i in {1..10}
+# do 
+# echo $i
+# done
+
+# for i in {1..10..2}
+# do 
+# echo $i
+# done
+
+# for (( i=0; i<=10; i=i+3 ))
+# do
+# echo $i
+# done
+
+
+# for i in f1_hello.sh f2_version.sh
+# do
+# cat $i
+# done
+
+
+# f1_hello=10
+# for i in ${f1_hello}=sh ${f2_version}=.sh
+# do
+#     echo $i
+# done
+
+
+
