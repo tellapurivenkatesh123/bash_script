d50f716988334cf07778dfb204c7d8e176f01520
tellapurivenkatesh123 <tellapurivenkateh6635@gmail.com>
2025-10-13 00:13:11 +0530

    all

---
+++ f26_readonly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+<< "end"
+readonly cammand does not allows to overwrite any declared variable or function
+end
+# readonly var=10
+# echo "$var"
+# var=5
+
+# function home() {
+# echo "hello"
+# }
+# readonly -f home # to declare a function as readonly "-f" flag is used
+
+# function home() {
+#     echo "hi"
+# }
+
---
+++ f27_signal_and_traps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+<< "end"
+signals and traps
+ctrl + c --> SIGINT
+ctrl + z --> SUSSPEND
+
+for more info
+"man 7 signal"
+
+for debugging
+use
+ bash -x ./file.sh
+ #!/bin/bash -x in sha bang
+ and 
+ set -x
+ :
+ :
+ :
+ set +x
+end
+touch nill.txt
+file=/home/venki/scripts/nill.txt
+
+trap " rm -f $file "  0 9 2 15
+echo "pid no: $$"
+function run() {
+    count=1
+    while [ $count -le 10 ]
+    do
+        sleep 3
+        echo "$count"
+        (( count++ ))
+    done
+    exit 0   
+}
+run
