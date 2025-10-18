b830b7165545ab831bfb571ec966ff8284a2e278
tellapurivenkatesh123 <tellapurivenkatesh6635@gmail.com>
2025-10-18 23:51:22 +0530

    all

--- git.sh
+++ git.sh
@@ -2,13 +2,15 @@
 echo "============================"
 echo "🚀 Automated Git Workflow"
 echo "============================"
-echo -e "press 'f' to commit files or 'ENTER' to continue:\c"
+echo -e "press ['f'] to commit files or ['ENTER'] to continue:\c"
 read input
 if [[ $input == "f" ]];then
     echo -e "Enter the files to commit:\c"
     read -a files 
     for f in ${files[@]};do
         if [ ! -f $f ];then 
+        echo "⚠️  File '$f' does not exist in the current directory."
+        echo "Exit  code -1"
         exit 1
         else
             continue
